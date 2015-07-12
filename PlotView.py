3bd463fb4063793e4a790167ae89a7fbda8db8c7
Yuxun  Zhang <yuxun.zhang14@imperial.ac.uk>
2015-07-12 20:56:46 +0100

    fix bugs for ploting function

--- PlotView.py
+++ PlotView.py
@@ -21,36 +21,35 @@ class PlotView:
         self.axes.grid(True)
         self.draw_axis()
         self.update()
-
-
-
-
+    
+    
+    
+    
     
     def remove_quantile_section(self, lower, upper):
         plt.cla()
     
     def set_visible(self,value):
         self.axes.set_visible(value)
-
+    
     def set_axis_size(self, width, height):
         pass
-
-
-
-
+    
+    
+    
+    
     def draw_landmark(self, x, y):
         self.axes.plot(x, y, 'or')
-    
+        self.update()
     
     def draw_axis(self):
         self.axes.set_xlim([0, self.nframes])
         self.axes.set_ylim([self.lower,self.upper])
         self.axes.set_title(self.desc)
         plt.axhline(0, color='black')
-        
         plt.yticks(np.linspace(-100,100,21))
         
-
+    
     
     def draw_quantile_section(self, lower, upper, level='', color='yellow'):
         xrange = [(0, self.nframes)]
@@ -60,14 +59,14 @@ class PlotView:
         c = collections.BrokenBarHCollection(xrange, yrange, facecolor=color, alpha = 0.1)
         self.axes.text(self.nframes/2, lower,level, fontsize=15, ha='center', alpha = 0.2)
         self.axes.add_collection(c)
-
-
+        self.update()
+    
     def draw_line(self, x1, y1, x2, y2):
         l =  Line2D([x1,x1],[y1,y2], linestyle='--',color = 'black')
         self.axes.add_line(l)
-
-
-
+        self.update()
+    
+    
     def draw_curve(self, landmark_list = [], _kind = 'cubic'):
         x = []
         y = []
@@ -78,23 +77,31 @@ class PlotView:
         
         xnew = np.linspace(min(x), max(x), max(x))
         self.axes.plot(xnew,f(xnew),'-')
-
+        self.update()
     
+    def draw_section_set(self, quantile_section):
+        for qs in quantile_section:
+            xrange = [(0, self.nframes)]
+            #yrange = ymin, ywidth
+            yrange = (qs[0], qs[1]-qs[0])
             
+            c = collections.BrokenBarHCollection(xrange, yrange, facecolor=qs[3], alpha = 0.1)
+            self.axes.text(self.nframes/2, qs[0],qs[2], fontsize=15, ha='center', alpha = 0.2)
+            self.axes.add_collection(c)
+    
     def draw(self, quantile_section=[], landmark=[]):
         plt.cla()
         self.draw_axis()
         self.axes.grid(True)
         #draw quantile sections
-        for qs in quantile_section:
-            self.draw_quantile_section(qs[0], qs[1], qs[2], qs[3])
+        self.draw_section_set(quantile_section)
         #draw landmark
         for la in landmark:
-            self.draw_landmark(la[0], la[1])
+            self.axes.plot(la[0], la[1], 'or')
         self.update()
-            
-            
-            
+
+
+
     def update(self):
         fig = plt.figure(self.id)
         fig.canvas.draw()
@@ -130,4 +137,4 @@ if __name__ == '__main__':
 
 
 
-    #view2.remove_quantile_section(50,80)
+#view2.remove_quantile_section(50,80)
